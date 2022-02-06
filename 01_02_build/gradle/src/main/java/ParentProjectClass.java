076263a2bc44bf7b317ef399e1a39af53dde235d
gneginskiy <gneginskiy@mail.ru>
2022-02-06 12:51:06 +0300

    initial commit

--- 01_02_build/gradle/src/main/java/ParentProjectClass.java
+++ 01_02_build/gradle/src/main/java/ParentProjectClass.java
@@ -1,7 +1,7 @@
 public class ParentProjectClass {
     public static void main(String[] args) {
         System.out.println("хм1");
-        ChildProjectClass1.main(new String[0]);
-        ChildProjectClass2.main(new String[0]);
+        ChildProjectClass1.main();
+        ChildProjectClass2.main();
     }
 }
